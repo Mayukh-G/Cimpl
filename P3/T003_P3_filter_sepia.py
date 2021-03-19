8ebceaf4055035009efdea71a389a79643783dc4
Artrus-104 <80218317+Artrus-104@users.noreply.github.com>
2021-03-19 16:38:33 -0400

    Update T003_P3_filter_sepia.py

--- P3/T003_P3_filter_sepia.py
+++ P3/T003_P3_filter_sepia.py
@@ -8,7 +8,7 @@ from simple_Cimpl_filters import grayscale
 #---------------------------------------------------------------------
 def sepia_filter(image: Image) -> Image:
     """
-    Returns a copy of the original image with the sepia filter applied.
+    Returns a copy of the original image with the sepia filter applied. 
     
     >>> file = choose_file()
     >>> image = load_image(file)
@@ -20,11 +20,17 @@ def sepia_filter(image: Image) -> Image:
     gray_image = grayscale(new_image)
     for pixel in gray_image:
         x,y, (r,g,b) = pixel
-        new_colour = create_color(r*1.15,g,b*0.85)
-        set_color(gray_image,x,y,new_colour)
-    
+        if r < 63: 
+            new_colour = create_color(r*1.1,g,b*0.9)
+            set_color(gray_image,x,y,new_colour)
+        elif 63 <= r <= 191:
+            new_colour = create_color(r*1.15,g,b*0.85)
+            set_color(gray_image,x,y,new_colour)
+        elif 191 < r <= 255:
+            new_colour = create_color(r*1.08,g,b*0.93)
+            set_color(gray_image,x,y,new_colour)            
+            
     return gray_image
-
 
 #Main Script
 #---------------------------------------------------------------------
