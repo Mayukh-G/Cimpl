4c2012263c43759db3d578fb30bf5557ede5c676
wattya13 <80218279+wattya13@users.noreply.github.com>
2021-03-20 17:48:35 -0400

    Update T003_P3_test_sepia.py

--- P3/T003_P3_test_sepia.py
+++ P3/T003_P3_test_sepia.py
@@ -6,7 +6,7 @@ StudentNum: 101178559
 from Cimpl import *
 from T003_P3_filter_sepia import sepia_filter
 from unit_testing import check_equal
-from simple_Cimpl_filters import grayscale
+
 
 
 def test_sepia() -> None:
@@ -16,21 +16,22 @@ def test_sepia() -> None:
     >>>test_sepia()
     
     """
-    original = create_image (3, 1) #Creating an original image to be tested  by the sepia filter.
+    original = create_image(3, 1) #Creating an original image to be tested  by the sepia filter.
     set_color(original, 0, 0,  create_color(45, 67, 233))
     set_color(original, 1, 0,  create_color(120, 27, 38))
     set_color(original, 2, 0,  create_color(255, 37, 54))    
     
-    expected = create_image(3, 1)
-    set_color(original, 0, 0,  create_color(132, 115, 98)) #Medium gray
-    set_color(original, 1, 0,  create_color(67, 61, 55)) #Light gray
-    set_color(original, 2, 0,  create_color(232, 215, 200))  #Dark gray 
+    expected1 = create_image(3, 1)
+    set_color(original, 0, 0,  create_color(132, 115, 97)) #Medium gray
+    set_color(original, 1, 0,  create_color(67, 61, 54)) #Light gray
+    set_color(original, 2, 0,  create_color(232, 215, 199))  #Dark gray 
     
     sepia_test = sepia_filter(original)
     for x, y, col in sepia_test:
         check_equal('Checking pixel @(' + str(x) + ', ' + str(y) + ')',
-                     col, get_color(expected, x, y))
+                     col, get_color(expected1, x, y))
         
 #Main Script
 test_sepia()
+    
     
