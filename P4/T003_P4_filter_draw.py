1921ddb5d66a37e6a129c823ae7bd884faac5650
Mayukh <mayukh.gautam@gmail.com>
2021-03-25 19:34:57 -0400

    Finished main funtion's body

--- P4/T003_P4_filter_draw.py
+++ P4/T003_P4_filter_draw.py
@@ -8,7 +8,7 @@ import numpy as npy
 import string
 
 
-def _interpolation():
+def _interpolation(coord_list: List[Tuple[int, int]]) -> List[int]:
     return None
 
 
@@ -16,7 +16,7 @@ def _exhaustive_search():
     return None
 
 
-def _image_border_finding():
+def _image_border_finding(img_width: int, img_height: int, coefficients: List[int]) -> List[Tuple[int, int]]:
     return None
 
 
@@ -32,11 +32,13 @@ COLOURS = (
     ("gray", Color(128, 128, 128))
 )
 
+Fx = npy.polyval
+
 
 def draw_curve(image: Image, col: str, coords: List[Tuple[int, int]] = None) -> Image:
     """
     Takes an Image object, a string that represents one of many colors and an optional list of coordinate tuples.
-    This function then draws an interpolated curve with a width of roughly 9 pixels using the coordinates provided.
+    This function then draws an interpolated curve with a width of 9 pixels using the coordinates provided.
     This function does not modify the original image.
 
     :param coords: If this argument is empty or None, this function will ask the user for coordinates. If not, this
@@ -56,22 +58,49 @@ def draw_curve(image: Image, col: str, coords: List[Tuple[int, int]] = None) -> 
 
     :returns: A copy of the original image passed in but with a curve drawn on it
     """
-    for colours in COLOURS:
+    img_c = image.copy()
+    for colours in COLOURS:  # Select correct Colour
         if colours[0] == col:
             chosen_col = colours[1]
 
     coord_internal = []
+    # UI for getting points
     if not coords:
         num_coords = input("How many coordinates would you like to input?\n")
         for x in range(int(num_coords)):
+            # Formats user input into a more tuple friendly format
             temp = input(f"Enter coordinate n.{x+1}. With this format: x,y\n").strip(string.punctuation).split(",")
+            # Changing the string to an integer
             for i in range(len(temp)):
                 temp[i] = int(temp[i])
-            coord_internal.append(temp)
-    print(coord_internal)
+            coord_internal.append(tuple(temp))
+
+    poly_coefficients = _interpolation(coord_internal)
+    edge_points = _image_border_finding(image.get_width(), image.get_height(), poly_coefficients)
+
+    border_x = []   # Only the x values of the border points
+    for point in edge_points:
+        border_x.append(point[0])
+
+    # Drawing curve
+    allow = True
+    for x in range(image.get_width()):
+        if (x not in border_x) and allow:  # This will only draw if the function is within range of the image
+            y = int(Fx(poly_coefficients, x))  # polyval returns a numpy.int32 type. This in incompatible with Cimpl
+            for j in range(9):  # Adding the thickness of the line. Only if it is within range of the image
+                r = y - 4 + j
+                if r in range(image.get_height()):
+                    img_c.set_color(x, r, chosen_col)
+        else:
+            # Checking if the next point is within range of the image
+            allow = 0 < Fx(poly_coefficients, x+1) < image.get_height()
+    return img_c
 
 
 if __name__ == '__main__':
-    draw_curve(Image(width=100, height=100), "black")
+    im = Image(filename=choose_file())
+    drawn = draw_curve(im, "black")
+    show(drawn)
+
 
 
