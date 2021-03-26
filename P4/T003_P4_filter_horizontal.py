465fd0e26ae34d7eedb156aa5d078ee7d1a46e65
Artrus-104 <80218317+Artrus-104@users.noreply.github.com>
2021-03-26 10:02:43 -0400

    Add files via upload

---
+++ P4/T003_P4_filter_horizontal.py
@@ -0,0 +1,29 @@
+# Author: Jacob Ridgway
+# Student ID: 101195221
+# Group No.: T003
+#---------------------------------------------------------------------
+from Cimpl import *
+from unit_testing import check_equal
+#---------------------------------------------------------------------
+def flip_horizontal(image: Image) -> Image:
+    """
+    Returns the orignal image flipped horizontally about the center axis.
+    """
+    
+    new_image = copy(image)
+    horz_image = new_image
+    
+    for pixel in image:
+        x,y,(r,g,b) = pixel
+        set_color(horz_image, -(x+1), y, create_color(r,g,b))
+            
+    return horz_image
+                        
+#---------------------------------------------------------------------
+# Main Script
+
+file = choose_file()
+image = load_image(file)
+horz_image = flip_horizontal(image)
+show(horz_image)
+       
