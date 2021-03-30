618e39ccd34630eb505a5e73dc5f44746c700f1d
Mayukh <mayukh.gautam@gmail.com>
2021-03-30 18:40:28 -0400

    Added P5

---
+++ P5/T003_batch_ui.py
@@ -0,0 +1,21 @@
+# Group  : T003
+# Course : ECOR1042G
+# Number : 101195221, 101181018
+# Names  : Jacob Ridgway, Mayukh Gautam
+
+# J : 3, X, T, P
+# M : E, D, V, H
+
+def get_args() -> list:
+    pass
+
+
+def parse_args() -> dict:
+    """
+    {"file": fn (Image), "save": sv (str), "number": n (int), "arguments": list ("str")}
+    """
+    pass
+
+
+def modify() -> None:
+    pass
