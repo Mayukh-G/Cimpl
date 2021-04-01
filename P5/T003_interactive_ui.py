c9059e72f07c3dc221db17cf81aee3cbb881b4e5
Alex-Christie-3 <80218283+Alex-Christie-3@users.noreply.github.com>
2021-04-01 16:48:27 -0400

    Update T003_interactive_ui.py

--- P5/T003_interactive_ui.py
+++ P5/T003_interactive_ui.py
@@ -21,3 +21,18 @@ comand_dict = {'L': load_image, 'S': save_as, '3': three_tone,
                
 
 # Main UI
+while comand != Q or q:
+    comand = str.capitalize(comand)
+    if comand_dict.get(comand) != None:
+        if type(image) == Image:
+            image = comand_dict.get(comand)(image)
+            
+        else: 
+            print("No image loaded")
+    else:
+        print("No such command")
+    
+    comand= input(" L)oad image    S)ave-as \n \
+    3)-tone    X)treme contrast    T)int sepia    P)osterize \n \
+    E)dge detect    D)raw curve    V)ertical filp    H)orizontal flip \n \
+    Q)uit \n \n : ")    
